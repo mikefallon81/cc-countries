7492792a3fbe5d596325916cdefecb92428c0055
lawrie-sm <alertrelic@gmail.com>
2019-08-06 13:34:16 +0100

    Got countries

--- src/app.js
+++ src/app.js
@@ -3,7 +3,14 @@ import Vue from 'vue';
 document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: '#app',
-    data: { message: 'hello' },
+    data: {
+      countries: [] 
+    },
+    mounted() {
+      this.countries = fetch('https://restcountries.eu/rest/v2/all')
+        .then(response => response.json())
+        .then(data => this.countries = data);
+    },
     computed: {},
     methods: {}
   });
